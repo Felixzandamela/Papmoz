27ba61504e37e06009f832c3fe13025f4421b106
Felixzandamela <81531283+Felixzandamela@users.noreply.github.com>
2023-10-03 16:59:13 +0200

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import App from './App';
 
-const root = createRoot(document.getElementById('app'));
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
 );
+
